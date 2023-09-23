6a24f389f11e18451b8da766f593a0151b59b89c
Artem Osadchyi <traibles@gmail.com>
2023-09-23 19:22:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿#region 1. Two Sum
+//int[] TwoSum(int[] nums, int target)
+//{
+//    //var temporary = new List<int>(nums);
+
+//    ////temporary = temporary.Where(x => x <= target).ToList();
+
+//    //for (int i = 0; i < temporary.Count; i++) 
+//    //{
+//    //    for (int j = i + 1; j < temporary.Count; j++)
+//    //    {
+//    //        if (temporary[i] + temporary[j] == target) 
+//    //        {
+//    //           return new int[] { i, j };
+//    //        }
+//    //    }
+//    //}
+//    //return new int[0];
+
+
+//    var Temporary = new List<int>();
+//    foreach (var item in nums)
+//        if (item < target)
+//            Temporary.Add(item);
+//    //Temporary.Sort();
+
+//    var Output = new List<int>();
+//    int Sum = 0;
+//    foreach (var item in Temporary)
+//    {
+//        if (Sum + item <= target)
+//        {
+//            Sum += item;
+//            Output.Add(item);
+//        }
+//    }
+
+//    return Output.Select(x => nums.ToList().IndexOf(x)).ToArray();
+
+
+//}
+
+//int[] nums = { 3, 2, 4 };
+
+//foreach (var temp in TwoSum(nums, 6))
+//    Console.WriteLine(temp);
+#endregion 1. Two Sum
+#region 9. Palindrome Number
+//bool IsPalindrome(int x)
+//{
+//    string temporary = x.ToString();
+//    string solution = String.Join("", temporary.Reverse());
+//    return solution == temporary;
+//}
+
+#endregion 9. Palindrome Number
