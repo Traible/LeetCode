369daa8d041d697945bb1eb7c9a1e6bc2acf135a
Артём Осадчий <traibles@gmail.com>
2024-06-23 19:26:07 +0200

    update: add #region 2073. Time Needed to Buy Tickets

--- Program.cs
+++ Program.cs
@@ -1284,3 +1284,33 @@ using static System.Net.Mime.MediaTypeNames;
 //return temp + 1;
 
 #endregion 268. Missing Number
+
+#region 231. Power of Two
+//int n = 8;
+//int[] arr = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824 };
+//Console.WriteLine(arr.Contains(n)); 
+#endregion
+
+#region 2073. Time Needed to Buy Tickets
+
+//int k = 2;
+//int[] tickets = { 2, 3, 2 };
+
+//int counter = 0;
+//while (true) 
+//{
+//    for (int i = 0; i < tickets.Length; i++)
+//    {
+//        if (tickets[i] > 0)
+//        {
+//            tickets[i] -= 1;
+//            counter++;
+//            if (tickets[k] == 0)
+//                Console.WriteLine(counter);
+//        }
+//        else
+//            continue;
+//    }
+//}
+
+#endregion
